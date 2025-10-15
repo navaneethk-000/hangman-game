ea39de3bdaea9de3c7ccabad11248f5ce2f97a52
Navaneeth <navaneethk969@gmail.com>
2025-10-15 15:10:32 +0530

    Adds program to get random words

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
 
-func getSecretWord(string) string {
-	return "navaneeth"
+func getSecretWord(wordFileName string) string {
+	allowedWords := []string{}
+	wordFile, err := os.Open(wordFileName)
+	if err != nil {
+		errMessage := fmt.Sprintf("Can't open file %s : %v\n", wordFileName, err)
+		panic(errMessage)
+	}
+	defer wordFile.Close()
+
+	scanner := bufio.NewScanner(wordFile)
+
+	for scanner.Scan() {
+
+		word := scanner.Text()
+		if word == strings.ToLower(word) {
+			allowedWords = append(allowedWords, word)
+		}
+	}
+
+	randomNum := rand.Intn(len(allowedWords))
+
+	return allowedWords[randomNum]
 }
 
 func main() {
