c49ead6aebc90813c19715fc530da834e114cff3
Navaneeth <navaneethk969@gmail.com>
2025-10-21 12:12:24 +0530

    GREEN : Implements function for game loss condition

--- main.go
+++ main.go
@@ -90,8 +90,38 @@ func hasWonGame(state Game) bool {
 	return true
 }
 
-func main() {
+func getGuess() byte {
+	fmt.Print("Guess a letter : ")
+	reader := bufio.NewReader(os.Stdin)
+	ch, _ := reader.ReadByte()
+	reader.ReadByte()
+	return ch
+}
 
+func hasLossGame(state Game) bool {
+	return state.chancesRemaining == 0
+}
+
+func main() {
 	secretWord := getSecretWord("/usr/share/dict/words")
-	fmt.Println(secretWord)
+	game := NewGame(secretWord)
+	fmt.Println("Welcome to Hangman!")
+
+	for game.chancesRemaining > 0 {
+
+		if hasWonGame(game) {
+			fmt.Println("You Won!")
+			break
+		}
+
+		fmt.Println("Chances remaining:", game.chancesRemaining)
+		fmt.Println("Guessed Letters : ", string(game.guessedLetters))
+
+		guess := getGuess()
+		game = checkGuess(game, guess)
+
+		if hasLossGame(game) {
+			fmt.Println("You Lose!")
+		}
+	}
 }
